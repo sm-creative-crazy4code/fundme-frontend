79c69e40af959844bda6fe9d14489d20b609ab9f
sneha_mandal <snehacreative2016@gmail.com>
2022-10-13 22:09:57 +0530

    adding functinalities

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function connectionMeta(){
            /**Web3Provider allows to wrap around stuff like metamask helping us to connect to the block chain
             * as we connect it to metamask it takes whatever end point the wallet is connected to and sticks it to our ethers
             */
-
+         try{
             const provider = new ethers.providers.Web3Provider(window.ethereum)
             // since wallet is connected to the network provider getsigner returns whatever wallet is connected to metamask
             const signer = provider.getSigner()//=>returns whichever wallet account is connected to the network provider
@@ -63,7 +63,43 @@ async function connectionMeta(){
             // once we have connected to our smart contract we can send transaction and interact with our smart contract
 
             const transactionResponse = await contract.fund({value:ethers.utils.parseEther(ethAmount)})
+         //we need to listen for the event of transaction getting mined and we stop till the function is completly mined
+        await listenForTransaction(transactionResponse,provider)
+        
+        
+        }catch(error){
+                console.log(error)
+            }
+
         }
+        
+
 
         
     }
+
+
+    function listenForTransaction(transactionResponse,provider){
+
+
+        console.log(`getting the transaction ${transactionResponse.hash}`)
+        //we will define how we are actually listening to this transaction using the event listeners provided by ethers ==>.once =>once a event fires we call the other function we have defined(it only triggers one time)
+        //the event we will wait for is the transaction receipt because it means that the transaction has completed
+
+        /**PROBLEM here our listener function will actually finish off before our provider.once function listsens for the confirmation and hence will go to the next line of execution and then due to the event loop in js it will come back again to execte the code
+         * hence we are adding the provider.once to the que called the event loop and periodically check back to it if its finished and hence we need to modify this function to resolve a promise
+         * promises by default take a functio as parameter 
+         * we pass in resolve and reject so that when we have successfully listened to a transaction we can actually resolve the promise
+         * once transaction hash is found we are calling the function and then
+         * so promise is only returned  and the function only finishes when resolve or reject is called
+         * then
+        */
+       return new Promise((resolve,reject)=>{
+        provider.once(transactionResponse.hash,(transactionReceipt)=>{
+th
+            console.log(`completed with ${transactionReceipt.confirmations}`)
+        })
+       })
+       
+    }
+    
